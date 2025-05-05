d6df41a00fff4fc98d487afab09a593fc4fa3a8b
filip <fifi.serbin@gmail.com>
2025-05-05 16:45:54 +0200

    Outputting to file

--- TestProject1/FirstScript.cs
+++ TestProject1/FirstScript.cs
@@ -5,26 +5,26 @@ using OpenQA.Selenium.Chrome;
 // No main method cause Top-level statements ¯\_(ツ)_/¯
 
 IWebDriver driver = new ChromeDriver();
+
+String textToWrite = "Failed to get prices";
 try
 {
     // Get USD/PLN
 
     driver.Navigate().GoToUrl("https://www.bankier.pl/waluty/kursy-walut/forex/USDPLN");
-    Console.WriteLine("Dollar Amerykański jest warty " + driver.FindElement(By.XPath("//div[@class='profilLast']")).Text + " PLN");
+    textToWrite = "Dollar Amerykański jest warty " + driver.FindElement(By.XPath("//div[@class='profilLast']")).Text + " PLN\n";
 
     //Get EUR/USD
 
     driver.Navigate().GoToUrl("https://www.bankier.pl/waluty/kursy-walut/forex/EURPLN");
-    Console.WriteLine("Euro jest warte " + driver.FindElement(By.XPath("//div[@class='profilLast']")).Text + " PLN");
+    textToWrite += "Euro jest warte " + driver.FindElement(By.XPath("//div[@class='profilLast']")).Text + " PLN";
 
-    // Get PLN/PLN
-
-    //driver.Navigate().GoToUrl("https://www.bankier.pl/waluty/kursy-walut/forex/USDEUR");
-    Console.WriteLine("Polskie złote jest warte 1 PLN");
 }
 catch 
 {
-    Console.WriteLine("Coś nie pykło");
+    Console.WriteLine("połączenie nie wyszło ups");
 }
+
+File.WriteAllText("C:/Users/fifis/Desktop/Kursy.txt", textToWrite);
 
 driver.Quit();
