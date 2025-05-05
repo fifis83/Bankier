62c494090f3c15091dd0d972df55894c111c5e20
filip <fifi.serbin@gmail.com>
2025-05-05 15:57:17 +0200

    Add project files.

---
+++ TestProject1/FirstScript.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+// No main method cause Top-level statements ¯\_(ツ)_/¯
+
+IWebDriver driver = new ChromeDriver();
+try
+{
+    // Get USD/PLN
+
+    driver.Navigate().GoToUrl("https://www.bankier.pl/waluty/kursy-walut/forex/USDPLN");
+    Console.WriteLine("Dollar Amerykański jest warty " + driver.FindElement(By.XPath("//div[@class='profilLast']")).Text + " PLN");
+
+    //Get EUR/USD
+
+    driver.Navigate().GoToUrl("https://www.bankier.pl/waluty/kursy-walut/forex/EURPLN");
+    Console.WriteLine("Euro jest warte " + driver.FindElement(By.XPath("//div[@class='profilLast']")).Text + " PLN");
+
+    // Get PLN/PLN
+
+    //driver.Navigate().GoToUrl("https://www.bankier.pl/waluty/kursy-walut/forex/USDEUR");
+    Console.WriteLine("Polskie złote jest warte 1 PLN");
+}
+catch 
+{
+    Console.WriteLine("Coś nie pykło");
+}
+
+driver.Quit();
